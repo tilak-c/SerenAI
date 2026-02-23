a9c98cd611ec75fce94650dc254f3a822e755821
Tilak <adireddytilak@gmail.com>
2026-02-23 16:07:28 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -18,24 +18,27 @@ const app = express();
 
 app.set("trust proxy", 1);  
 
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://seren-ai-vshy.vercel.app",
+//   "https://seren-ai-amber.vercel.app" 
+// ];
 const allowedOrigins = [
-  "http://localhost:5173",
-  "https://seren-ai-vshy.vercel.app",
-  "https://seren-ai-amber.vercel.app" 
+  "https://seren-ai-vshy.vercel.app"
 ];
 
 app.use(cors({
   origin: function (origin, callback) {
-
     if (!origin) return callback(null, true);
 
-    if (allowedOrigins.includes(origin)) {
+    if (
+      allowedOrigins.includes(origin) ||
+      origin.endsWith(".vercel.app")
+    ) {
       callback(null, true);
-    }
-    else {
+    } else {
       callback(new Error("Not allowed by CORS"));
     }
-
   },
   credentials: true
 }));
